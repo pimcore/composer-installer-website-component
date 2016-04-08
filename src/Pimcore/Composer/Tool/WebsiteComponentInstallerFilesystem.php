5f9197a8ec4bd79879a612d4a019ef2312b9f786
Bernhard Rusch <brusch@users.noreply.github.com>
2016-04-08 13:41:05 +0200

    Update WebsiteComponentInstallerFilesystem.php

--- src/Pimcore/Composer/Tool/WebsiteComponentInstallerFilesystem.php
+++ src/Pimcore/Composer/Tool/WebsiteComponentInstallerFilesystem.php
@@ -1,1 +1,15 @@
 <?php 
+namespace Pimcore\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class WebsiteComponentInstallerFilesystem extends \Composer\Util\Filesystem
+{
+    public function copyThenRemove($source, $target)
+    {
+        echo "-----> " . $target . "\n"; 
+        
+        parent::copyThenRemove($source, $target); 
+    }
+}
