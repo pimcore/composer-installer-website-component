12e99496cb4146a16c50eb29738fc7ca20ce1e4b
Bernhard Rusch <brusch@users.noreply.github.com>
2016-04-08 14:51:43 +0200

    Update WebsiteComponentInstallerFilesystem.php

--- src/Pimcore/Composer/Tool/WebsiteComponentInstallerFilesystem.php
+++ src/Pimcore/Composer/Tool/WebsiteComponentInstallerFilesystem.php
@@ -11,6 +11,35 @@ class WebsiteComponentInstallerFilesystem extends \Composer\Util\Filesystem
         fwrite($f, "source: " . $source . "\n" . "target: " . $target . "\n----------\n");
         fclose($f);
         
-        //parent::copyThenRemove($source, $target); 
+        parent::copyThenRemove($source, $target); 
+    }
+    
+    
+    public function isDirEmpty($dir) {
+        return true; 
+    }
+    
+    public function emptyDirectory($dir, $ensureDirectoryExists = true) {
+        
+    }
+    
+    public function removeDirectory($directory) {
+        
+    }
+    
+    public function removeDirectoryPhp($directory) {
+        
+    }
+    
+    public function unlink($path) {
+        return true;    
+    }
+    
+    public function rmdir($path) {
+        return true; 
+    }
+    
+    public function remove($file) {
+        return true; 
     }
 }
