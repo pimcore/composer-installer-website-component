f0d1ca5a96e9bdbc5d1692f415268b0386621877
Bernhard Rusch <brusch@users.noreply.github.com>
2016-04-11 16:10:21 +0200

    Update WebsiteComponentInstaller.php

--- src/Pimcore/Composer/WebsiteComponentInstaller.php
+++ src/Pimcore/Composer/WebsiteComponentInstaller.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 
 namespace Pimcore\Composer;
 
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
-use Composer\IO\IOInterface; 
-use Composer\Composer; 
-use Composer\Util\Filesystem; 
+use Composer\IO\IOInterface;
+use Composer\Composer;
+use Composer\Util\Filesystem;
 use Composer\Downloader\DownloadManager;
 use Composer\Repository\InstalledRepositoryInterface;
 use Composer\Util\Silencer;
@@ -18,38 +18,38 @@ class WebsiteComponentInstaller extends LibraryInstaller
      */
     public function getInstallPath(PackageInterface $package)
     {
-	    $docRootName = "./"; 
-	    if($configDocRoot = $this->composer->getConfig()->get("document-root-path")) {
-	        $docRootName = rtrim($configDocRoot,"/");
-	    }
-		
+        $docRootName = "./";
+        if ($configDocRoot = $this->composer->getConfig()->get("document-root-path")) {
+            $docRootName = rtrim($configDocRoot, "/");
+        }
+
         return $docRootName . "/";
     }
 
     protected function installCode(PackageInterface $package)
     {
-    	
-    	$downloadPath = $this->getInstallPath($package) . "website/var/system/composer-website-" . uniqid() . "/";
-    	mkdir($downloadPath, 0777);
+
+        $downloadPath = $this->getInstallPath($package) . "website/var/system/composer-website-" . uniqid() . "/";
+        mkdir($downloadPath, 0777);
         $this->downloadManager->download($package, $downloadPath);
-        
+
         $targetPath = $this->getInstallPath($package);
         foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($downloadPath)) as $file) {
-            if(is_file($file->getPathName())) {
+            if (is_file($file->getPathName())) {
                 $relativePath = preg_replace("@^" . preg_quote($downloadPath, "@") . "@", "", $file->getPathName());
-                if(preg_match("@^website/@", $relativePath)) {
+                if (preg_match("@^website/@", $relativePath)) {
                     $targetFile = $targetPath . $relativePath;
-                    if(!is_dir(dirname($targetFile))) {
+                    if (!is_dir(dirname($targetFile))) {
                         mkdir(dirname($targetFile), 0755, true);
                     }
 
-		            if(!file_exists($targetFile)) {
+                    if (!file_exists($targetFile)) {
                         copy($file->getPathName(), $targetFile);
-		            }
+                    }
                 }
             }
         }
-        
+
         // cleanup tmp
         $files = new \RecursiveIteratorIterator(
             new \RecursiveDirectoryIterator($downloadPath, \RecursiveDirectoryIterator::SKIP_DOTS),
@@ -71,12 +71,12 @@ class WebsiteComponentInstaller extends LibraryInstaller
 
     protected function removeCode(PackageInterface $package)
     {
-    	// foo
+        // foo
     }
-    
-    protected function removeBinaries(PackageInterface $package) 
+
+    protected function removeBinaries(PackageInterface $package)
     {
-    	// foo
+        // foo
     }
 
     /**
